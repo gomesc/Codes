b52252fb7309714ef6c0025633bfe74be9f56889
gomesc <caio.luiz1@gmail.com>
2016-03-23 16:14:27 -0300

    Foi removido a função de cálculo da corrente

--- multi_e_ampe/m_e_a.ino
+++ multi_e_ampe/m_e_a.ino
@@ -19,10 +19,6 @@ float voltage_func(int a_read_v){
 	return (analogRead(a_read_v)*aRef) / 1023.0 * ((r1+r2)/r2);
 }
 
-// Ammeter Function
-float amper_func(int a_read_i){
-	
-}
 void setup(){
 }
 
