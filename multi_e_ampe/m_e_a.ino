fe7849bb1dcab0cc9f6fd8679255acff49b648e1
gomesc <caio.luiz1@gmail.com>
2016-04-27 12:22:44 -0300

    Feito parte do código do display

--- multi_e_ampe/m_e_a.ino
+++ multi_e_ampe/m_e_a.ino
@@ -9,6 +9,12 @@
 #define r2       100000;
 #define aRef     5;
 #define time_ref 500;
+#define a_read_v    10;   //Pino da tensão
+#define a_read_i    11;   //Pino da corrente
+
+//Declaração de portas
+int a_1[] = {2,3,4,5};
+int a_2[] = {6,7,8,9};
 
 // Global variable
 float voltage;
@@ -20,10 +26,20 @@ float voltage_func(int a_read_v){
 }
 
 // Ammeter Function
-float amper_func(int a_read_i){
-	
+float amper_func(int a_read_i)
+{
+
 }
-void setup(){
+void setup()
+{
+	for (int pin = 0; pin < 4; pin++)
+	{
+		pinMode(a_1[pin],OUTPUT);
+		pinMode(a_2[pin],OUTPUT);
+	}
+	
+	pinMode(pin_v,INPUT);
+	pinMode(pin_i,INPUT);
 }
 
 void loop(){
@@ -34,4 +50,19 @@ void loop(){
 	float v = voltage;
 
 	//Falta mostrar o valor da tensão
+}
+
+void display_7(byte num)
+{
+	for(int i = 0; i < 4; i++)
+	{
+		if(bitRead(num,i)==1)
+		{
+			digitalWrite(a_1[i],HIGH);
+		}
+		else
+		{
+			digitalWrite(a_1[i],LOW);
+		}
+	}
 }
